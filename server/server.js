1a6d15756a4046c83f347f4e16119e3475c851c0
nowaveosu <nowaveosu@gmail.com>
2024-05-07 20:11:41 +0900

    remove gitignore files

--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
+const io = require("socket.io")(3000,{
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+},
+});
+
+io.on("connection",(socket) => {
+    console.log("A user connected.")
+})
+
 console.log("hello world")
