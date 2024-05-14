ff0c073dab6276ce28567e5fa67be69e733c25df
nowaveosu <nowaveosu@gmail.com>
2024-05-14 18:01:05 +0900

    [fix] 틱택토 칸크기 늘림

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -4,76 +4,77 @@ import React, { useEffect, useState } from 'react';
 import { io } from "socket.io-client";
 
 export default function Home() {
-    const [socket, setSocket] = useState<any>(undefined);
-    const [inbox, setInbox] = useState<string[]>([]);
-    const [message, setMessage] = useState("");
-    const [roomName, setRoomName] = useState("");
-    const [gameState, setGameState] = useState<any>(undefined);
+  const [socket, setSocket] = useState<any>(undefined)
+  const [inbox, setInbox] = useState<string[]>([])
+  const [message, setMessage] = useState("")
+  const [roomName, setRoomName] = useState("")
+  const [gameState, setGameState] = useState<any>(undefined);
+  
+  const handleSendMessage = () => {
+    socket.emit("message",message, roomName)
+  }
+  
+  const handleJoinRoom= () => {
+    socket.emit("joinRoom", roomName)
+  }
 
-    const handleSendMessage = () => {
-    socket.emit("message", message, roomName);
-    };
-
-    const handleJoinRoom = () => {
-    socket.emit("joinRoom", roomName);
-    };
-
-    const handleMakeMove = (index: number) => {
+  const handleMakeMove = (index: number) => {
     socket.emit("makeMove", index, roomName);
-    };
+  }
+  
+  useEffect(() =>{
+    const socket = io('http://localhost:3000')
+    setSocket(socket)
+  },[])
 
-    useEffect(() => {
-    const socket = io('http://localhost:3000');
-    setSocket(socket);
-    }, []);
-
-    useEffect(() => {
+  useEffect(() => {
     if (socket) {
-        socket.on('message', (message: string) => {
-        setInbox((prevInbox) => [...prevInbox, message]);
-        });
+      socket.on('message',(message: string) => {
+        setInbox((prevInbox) => [...prevInbox, message]) 
+      })
 
-        socket.on('gameState', (gameState: any) => {
+      socket.on('gameState', (gameState: any) => {
         setGameState(gameState);
-        });
+      })
     }
-    }, [socket]);
+  }, [socket]) 
 
-    return (
+  return (
     <div>
-        <div className="flex flex-col gap-5 mt-20 px-10 lg:px-48">
-        <div className="flex flex-wrap justify-center">
-            {gameState && Array.from({ length: 3 }).map((_, rowIndex) => (
-            <div key={rowIndex} className="flex justify-center w-full">
-                {gameState.board.slice(rowIndex * 3, (rowIndex + 1) * 3).map((cell: string, cellIndex: number) => (
-                <div key={rowIndex * 3 + cellIndex} className="border rounded px-4 py-2 m-1 w-8 h-8 flex items-center justify-center" onClick={() => handleMakeMove(rowIndex * 3 + cellIndex)}>
-                    {cell}
+      <div className="flex flex-col gap-5 mt-20 px-10 lg:px-48">
+      <div className='flex flex-wrap justify-center'>
+          {gameState && Array.from({ length: 3 }).map((_, rowIndex) => (
+            <div key = {rowIndex} className='flex justify-center w-full'>
+              {gameState.board.slice(rowIndex * 3, (rowIndex + 1) * 3).map((cell: string, cellIndex: number) => (
+                <div key={rowIndex * 3 + cellIndex} className = "border rounded px-4 py-2 m-1 w-10 h-10 flex items-center justify-center" onClick={() => handleMakeMove(rowIndex * 3 + cellIndex)} >
+                  {cell}
                 </div>
-                ))}
+              ))}
             </div>
-            ))}
-        </div>
+          ))}
+      </div>
 
-        <div className="flex flex-col gap-2 border rounded-lg p-10">
-            {inbox.map((message: string, index: number) => (
-            <div key={index} className="border rounded px-4 py-2">{message}</div>
-            ))}
-        </div>
+      <div className="flex flex-col gap-2 border rounded-lg p-10">
+        {inbox.map((message: string, index: number) => (
+          <div key={index} className="border rounded px-4 py-2">{message}</div>
+        ))}
+      </div>
+      
+      <div className="flex gap-2 align-center justify-center">
+        <input onChange={(e) => {
+          setMessage(e.target.value)
+        }}type="text" name="message" className="flex-1 bg-black border rounded px-2 py-1"/>
+        <button className="w-40" onClick={handleSendMessage}>Send message</button>
+      </div>
+      
+      <div className="flex gap-2 align-center justify-center">
+        <input onChange={(e) => {
+          setRoomName(e.target.value)
+        }}type="text" name="room" className="flex-1 bg-black border rounded px-2 py-1"/>
+        <button className="w-40" onClick={handleJoinRoom}>Join Room</button>
+      </div>
 
-        <div className="flex gap-2 align-center justify-center">
-            <input onChange={(e) => {
-            setMessage(e.target.value);
-            }} type="text" name="message" className="flex-1 bg-black border rounded px-2 py-1"/>
-            <button className="w-40" onClick={handleSendMessage}>Send message</button>
-        </div>
-
-        <div className="flex gap-2 align-center justify-center">
-            <input onChange={(e) => {
-            setRoomName(e.target.value);
-            }} type="text" name="room" className="flex-1 bg-black border rounded px-2 py-1"/>
-            <button className="w-40" onClick={handleJoinRoom}>Join Room</button>
-        </div>
-        </div>
     </div>
-    );
+    </div>
+  );
 }
