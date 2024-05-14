bb597c9b21d37b5117ca35b383a7bad25a73262a
nowaveosu <nowaveosu@gmail.com>
2024-05-14 17:56:46 +0900

    [fix] 따옴표 통일

--- server/server.js
+++ server/server.js
@@ -1,62 +1,60 @@
-const io = require("socket.io")(3000,{
+const io = require("socket.io")(3000, {
     cors: {
         origin: "http://localhost:3001",
         methods: ["GET", "POST"],
-},
+    },
 });
 
 let rooms = {};
 
-io.on("connection",(socket) => {
-    console.log("A user connected.")
+io.on("connection", (socket) => {
+    console.log("A user connected.");
 
     socket.on("joinRoom", (roomName) => {
-        console.log("joining room : " + roomName);
+        console.log("joining room: " + roomName);
         socket.join(roomName);
         if (!rooms[roomName]) {
             rooms[roomName] = {
                 players: [],
                 board: Array(9).fill(null),
                 turn: 0,
-            }
+            };
         }
         rooms[roomName].players.push(socket.id);
         io.to(roomName).emit("gameState", rooms[roomName]);
-    })
+    });
 
-    socket.on("message",(message, roomName) => {
-    console.log("sending message", message, roomName)
-        if(roomName.length){
-            io.to(roomName).emit("message", message)
+    socket.on("message", (message, roomName) => {
+        console.log("sending message", message, roomName);
+        if (roomName.length) {
+            io.to(roomName).emit("message", message);
         } else {
-            io.emit("message",message)
+            io.emit("message", message);
         }
-        
-    })
-    
+    });
+
     socket.on("makeMove", (index, roomName) => {
         let room = rooms[roomName];
         if (room && room.players[room.turn % 2] === socket.id && room.board[index] === null) {
             room.board[index] = room.turn % 2 === 0 ? "X" : "O";
             room.turn++;
-            io.to(roomName).emit("ga")
+            io.to(roomName).emit("gameState", room);
         }
-    })
+    });
 
     socket.on("disconnect", () => {
         console.log("user disconnected.");
-        for(let roomName in rooms) {
+        for (let roomName in rooms) {
             let room = rooms[roomName];
             let playerIndex = room.players.indexOf(socket.id);
-            if(playerIndex !== -1) {
+            if (playerIndex !== -1) {
                 room.players.splice(playerIndex, 1);
-                if(room.players.length === 0) {
+                if (room.players.length === 0) {
                     delete rooms[roomName];
                 } else {
-                    io.to(roomName).emit("gmaeState", room);
+                    io.to(roomName).emit("gameState", room);
                 }
             }
         }
-    })
-
-})
+    });
+});
