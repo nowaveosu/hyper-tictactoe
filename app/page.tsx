00d08a6ff8912866bf6c96544796ff5d52621a41
nowaveosu <nowaveosu@gmail.com>
2024-05-25 00:07:24 +0900

    [Delete] console.log 제거

--- app/page.tsx
+++ app/page.tsx
@@ -106,7 +106,7 @@ export default function Home() {
         });
         
         socket.on("gameState", (newGameState: any) => {
-          console.log("Client - received oldestIndex:", newGameState.oldestIndex);  
+
           setGameState((prevGameState:any) => ({
               ...prevGameState,
               ...newGameState,
